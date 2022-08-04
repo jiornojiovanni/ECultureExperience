657da0372b0c787fe4ec32e8f41bc73f42912ad2
NickPom <nikomatri@gmail.com>
2022-08-04 16:17:44 +0200

    Changes Test

--- app/src/main/java/com/jannuzzi/ecultureexperience/MainActivity.java
+++ app/src/main/java/com/jannuzzi/ecultureexperience/MainActivity.java
@@ -32,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
         binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Juan è fess", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
