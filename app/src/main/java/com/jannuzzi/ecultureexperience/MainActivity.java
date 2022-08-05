682d7114f7a32e4f205706aa0d250a61114cf1da
NickPom <nikomatri@gmail.com>
2022-08-05 16:19:41 +0200

    Added LoginActivity

--- app/src/main/java/com/jannuzzi/ecultureexperience/MainActivity.java
+++ app/src/main/java/com/jannuzzi/ecultureexperience/MainActivity.java
@@ -14,6 +14,7 @@ import androidx.navigation.ui.NavigationUI;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import com.jannuzzi.ecultureexperience.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
