1649e6d797a78d21981193bb0e27a8ae386ddc6d
FVitale27 <f.vitale29@studenti.uniba.it>
2022-08-08 12:20:19 +0200

    Register update

---
+++ app/src/main/java/com/jannuzzi/ecultureexperience/ui/register/RegisterActivity.java
@@ -0,0 +1,14 @@
+package com.jannuzzi.ecultureexperience.ui.register;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.jannuzzi.ecultureexperience.R;
+
+public class RegisterActivity extends AppCompatActivity {
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
