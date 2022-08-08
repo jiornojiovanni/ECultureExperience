83f180ee5f3b0976801779557405b637926637ea
FVitale27 <f.vitale29@studenti.uniba.it>
2022-08-08 17:23:12 +0200

    Sign_Up page ended

--- app/src/main/java/com/jannuzzi/ecultureexperience/ui/register/RegisterActivity.java
+++ app/src/main/java/com/jannuzzi/ecultureexperience/ui/register/RegisterActivity.java
@@ -1,14 +1,32 @@
 package com.jannuzzi.ecultureexperience.ui.register;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.jannuzzi.ecultureexperience.R;
+import com.jannuzzi.ecultureexperience.ui.login.LoginActivity;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    TextView textView;
+
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        textView=(TextView)findViewById(R.id.login);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(RegisterActivity.this,LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
 }
