05d727e79671be4ab561ec6f7dbf2a388c3b3967
FVitale27 <f.vitale29@studenti.uniba.it>
2022-08-11 16:25:41 +0200

    Card event created

---
+++ app/src/main/java/com/jannuzzi/ecultureexperience/ui/route/RouteActivity.java
@@ -0,0 +1,19 @@
+package com.jannuzzi.ecultureexperience.ui.route;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.jannuzzi.ecultureexperience.R;
+
+public class RouteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_route);
+
+
+
+
+    }
+}
