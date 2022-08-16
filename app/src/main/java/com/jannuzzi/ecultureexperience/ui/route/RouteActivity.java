8cdc54b83ade1fe90dfd326b7c11186518edda63
FVitale27 <f.vitale29@studenti.uniba.it>
2022-08-16 11:26:34 +0200

    path card initialized

--- app/src/main/java/com/jannuzzi/ecultureexperience/ui/route/RouteActivity.java
+++ app/src/main/java/com/jannuzzi/ecultureexperience/ui/route/RouteActivity.java
@@ -1,19 +1,27 @@
 package com.jannuzzi.ecultureexperience.ui.route;
-
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.cardview.widget.CardView;
+import android.view.View;
+import android.content.ClipData;
 import android.os.Bundle;
 import com.jannuzzi.ecultureexperience.R;
 
 public class RouteActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_route);
+        setContentView(R.layout.row_path);
 
-
-
-
+        final CardView cardView = findViewById(R.id.card_event);
+        cardView.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View view) {
+                cardView.setChecked(!cardView.isChecked());
+                cardView.toggle();
+            }
+        });
     }
+
+
 }
