bf0a8ad30610984477e5d248b8e2742f4d7a3629
Giovanni Palmieri <gianni00palmieri@gmail.com>
2022-08-16 12:14:50 +0200

    added onclick listener to card_event

--- app/src/main/java/com/jannuzzi/ecultureexperience/ui/route/RouteActivity.java
+++ app/src/main/java/com/jannuzzi/ecultureexperience/ui/route/RouteActivity.java
@@ -1,9 +1,11 @@
 package com.jannuzzi.ecultureexperience.ui.route;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.cardview.widget.CardView;
-import android.view.View;
-import android.content.ClipData;
+
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.card.MaterialCardView;
 import com.jannuzzi.ecultureexperience.R;
 
 public class RouteActivity extends AppCompatActivity {
@@ -14,11 +16,12 @@ public class RouteActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.row_path);
 
-        final CardView cardView = findViewById(R.id.card_event);
-        cardView.setOnClickListener(new View.OnClickListener() {
-            @Override public void onClick(View view) {
-                cardView.setChecked(!cardView.isChecked());
-                cardView.toggle();
+        MaterialCardView view = findViewById(R.id.card_event);
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MaterialCardView card = (MaterialCardView) view;
+                card.setChecked(!card.isChecked());
             }
         });
     }
