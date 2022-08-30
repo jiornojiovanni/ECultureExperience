17124a3365a52049b5bc74d6be5c10a01d749374
Giovanni Palmieri <gianni00palmieri@gmail.com>
2022-08-30 12:53:26 +0200

    loaded route from file

--- app/src/main/java/com/jannuzzi/ecultureexperience/ui/route/RouteActivity.java
+++ app/src/main/java/com/jannuzzi/ecultureexperience/ui/route/RouteActivity.java
@@ -1,12 +1,24 @@
 package com.jannuzzi.ecultureexperience.ui.route;
 
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.JsonReader;
+import android.util.Log;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.card.MaterialCardView;
 import com.jannuzzi.ecultureexperience.R;
+import com.jannuzzi.ecultureexperience.data.model.Route;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class RouteActivity extends AppCompatActivity {
 
@@ -15,6 +27,11 @@ public class RouteActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.row_path);
+
+        String pathFile = getIntent().getExtras().getString("pathFile");
+        InputStream fileContent = readPathFile(pathFile);
+        List<Route> instructions = parsePathFile(fileContent);
+        Log.w("path", instructions.toString());
 
         MaterialCardView view = findViewById(R.id.card_event);
         view.setOnClickListener(new View.OnClickListener() {
@@ -26,5 +43,47 @@ public class RouteActivity extends AppCompatActivity {
         });
     }
 
+    private InputStream readPathFile(String name) {
+        File file = new File(Environment.getExternalStorageDirectory()
+                + "/Download/" + name);
+        try {
+            return new FileInputStream(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 
+    private List<Route> parsePathFile(InputStream content) {
+        JsonReader reader = new JsonReader(new InputStreamReader(content));
+        List<Route> instructions = new ArrayList<>();
+        try {
+            reader.beginArray();
+
+            while (reader.hasNext()) {
+                String title = "";
+                String description = "";
+
+                reader.beginObject();
+                while (reader.hasNext()) {
+                    String token = reader.nextName();
+                    if ("title".equals(token)) {
+                        title = reader.nextString();
+                    } else if ("description".equals(token)) {
+                        description = reader.nextString();
+                    } else {
+                        reader.skipValue();
+                    }
+                }
+
+                reader.endObject();
+                instructions.add(new Route(title, description));
+            }
+
+            return instructions;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return instructions;
+        }
+    }
 }
