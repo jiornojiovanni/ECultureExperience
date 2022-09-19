b528bdf118d99f002b9d92a79bf1be01f4802621
Giovanni Palmieri <gianni00palmieri@gmail.com>
2022-09-19 17:59:33 +0200

    Bluetooth

---
+++ app/src/main/java/com/jannuzzi/ecultureexperience/ui/BluetoothActivity.java
@@ -0,0 +1,122 @@
+package com.jannuzzi.ecultureexperience.ui;
+
+import static android.bluetooth.BluetoothAdapter.ACTION_DISCOVERY_STARTED;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.jannuzzi.ecultureexperience.R;
+import com.jannuzzi.ecultureexperience.ui.game.StartUp;
+
+import java.util.Set;
+
+public class BluetoothActivity extends AppCompatActivity {
+
+    private static final int REQUEST_ENABLE_BT = 1240101;
+    private static final int BLUETOOTH_PERMISSION = 3488223;
+    Button bluetooth_button;
+    BluetoothAdapter bluetoothAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bluetooth);
+        bluetooth_button = findViewById(R.id.bluetooth_button);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        BluetoothManager bluetoothManager = getSystemService(BluetoothManager.class);
+        bluetoothAdapter = bluetoothManager.getAdapter();
+        if (bluetoothAdapter == null) {
+            Toast.makeText(this, R.string.error_loading, Toast.LENGTH_LONG).show();
+            finish();
+        }
+        bluetooth_button.setOnClickListener(v -> {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) == PackageManager.PERMISSION_GRANTED
+            || ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH) == PackageManager.PERMISSION_GRANTED) {
+                activateBluetooth();
+                findGame();
+            } else {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                    ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.BLUETOOTH_CONNECT}, BLUETOOTH_PERMISSION);
+                } else {
+                    ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.BLUETOOTH}, BLUETOOTH_PERMISSION);
+                }
+            }
+        });
+    }
+
+    private void findGame() {
+        Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
+
+        if (pairedDevices.size() > 0) {
+            for (BluetoothDevice device : pairedDevices) {
+                String deviceName = device.getName();
+                if(deviceName.equals("gioco")) {
+                    Toast.makeText(this, R.string.device_found, Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(this, StartUp.class);
+                    startActivity(intent);
+                }
+            }
+        }
+
+        Toast.makeText(this, R.string.not_found, Toast.LENGTH_SHORT).show();
+    }
+
+    private void activateBluetooth() {
+        if (!bluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                    ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.BLUETOOTH_CONNECT}, BLUETOOTH_PERMISSION);
+                }
+            }
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == BLUETOOTH_PERMISSION) {
+            if (grantResults.length > 0 &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, R.string.bluetooth_success, Toast.LENGTH_SHORT).show();
+                activateBluetooth();
+                findGame();
+            } else {
+                Toast.makeText(this, R.string.activate_bluetooth, Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_ENABLE_BT && resultCode == RESULT_OK) {
+            findGame();
+        } else {
+            Toast.makeText(this, R.string.activate_bluetooth, Toast.LENGTH_SHORT).show();
+        }
+    }
+}
